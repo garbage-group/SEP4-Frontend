768a3e770364b16e2df4121c84ac51cb9c1af123
saransing <325611@via.dk>
2023-11-14 09:02:13 +0100

    feat(humidity-value-display): - Created HumidityDisplay component - Created stylesheet for HumidityDisplay component - Added get request to fetch humidity level. References #8

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import HumidityDisplay from './components/HumidityDisplay';
+import './styles/HumidityDisplay.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* Other content */}
+                <HumidityDisplay />
+            </header>
+        </div>
+    );
+}
+export default App;
+
