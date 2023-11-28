2faba2c0f4bd0306505ea623af559c1e42d5c017
rajipkanxo01 <pdlrajip12@gmail.com>
2023-11-28 13:59:04 +0100

    feat(list of collector in overview) - created design to show list of collectors in overview page
    References Show list of garbage collectors #28

--- src/components/overview/ListofCollectors.js
+++ src/components/overview/ListofCollectors.js
@@ -1,7 +1,68 @@
 import React from "react";
 
+import {
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+} from "@mui/material";
+import { MoreVertOutlined } from "@mui/icons-material";
+import stc from "string-to-color";
+
 import "../../styles/overview_css/ListOfCollectors.css";
 
+function stringToColor(string) {
+  const colour = stc(string);
+  return colour;
+}
+
+function stringAvatar(fullname) {
+  const bgColor = { bgcolor: stringToColor(fullname) };
+  const [firstName, lastName] = fullname.split(" ");
+  const initials = firstName[0] + (lastName ? lastName[0] : "");
+
+  return { sx: bgColor, children: initials };
+}
+
+const collectorsList = [
+  { fullname: "Rajib Paudyal", username: "rajipkanxo01" },
+  { fullname: "Suhani Pandey", username: "iamSuhani5" },
+  { fullname: "Pramesh Shrestha", username: "prmsstha" },
+];
+
 export function ListOfCollectors() {
-  return <div className="list-container"></div>;
+  return (
+    <div className="list-container">
+      <div className="title-container">
+        <p>Garbage Collectors</p>
+        <p>View All Users</p>
+      </div>
+      <List>
+        {collectorsList.map((collector, index) => (
+          <CollectorComponent key={index} {...collector} />
+        ))}
+      </List>
+    </div>
+  );
+}
+
+function CollectorComponent({ fullname, username }) {
+  return (
+    <ListItem
+      className="listitem-collector"
+      style={{ width: "auto" }}
+      secondaryAction={
+        <IconButton edge="end">
+          <MoreVertOutlined />
+        </IconButton>
+      }
+    >
+      <ListItemAvatar>
+        <Avatar {...stringAvatar(fullname)} />
+      </ListItemAvatar>
+      <ListItemText primary={fullname} secondary={`@${username}`} />
+    </ListItem>
+  );
 }
