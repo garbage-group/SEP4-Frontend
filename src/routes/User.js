fc59a3b0a49baa308b92f535c7f0fa884ca0268a
rajipkanxo01 <pdlrajip12@gmail.com>
2023-11-29 00:29:36 +0100

    feat(list header design done) - still need to fix some css
    References Show list of garbage collectors #28

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,7 +1,44 @@
 import React from "react";
 
-const Users = () => {
-  return <div className="users-container"></div>;
-};
+import { Button } from "@mui/material";
+import PersonAddAltRoundedIcon from "@mui/icons-material/PersonAddAltRounded";
+
+import "../styles/user_css/User.css";
+
+function Users() {
+  return (
+    <div className="users-container">
+      <UserListContainer />
+      <FilterContainer />
+    </div>
+  );
+}
+
+function UserListContainer() {
+  return (
+    <div className="userlist-container">
+      <div className="list-header">
+        <p className="members-text">Garbage Collectors</p>
+
+        <div className="right-content">
+          <p className="number-of-users"> 13 users </p>
+          <Button
+            variant="contained"
+            className="add-member-button"
+            endIcon={<PersonAddAltRoundedIcon />}
+          >
+            Add User
+          </Button>
+        </div>
+      </div>
+
+      <div className="list-body"></div>
+    </div>
+  );
+}
+
+function FilterContainer() {
+  return <div className="filter-container"></div>;
+}
 
 export { Users };
