dedea8bfc881647bdacc55cafbbe1901c770ba2f
rajipkanxo01 <pdlrajip12@gmail.com>
2023-12-03 21:47:39 +0100

    fix (Navbar.test.js and HumidityDisplay.test.js test cases fixed)

--- src/test/HumidityDisplay.test.js
+++ src/test/HumidityDisplay.test.js
@@ -1,65 +1,84 @@
-// import React from 'react';
-// import '@testing-library/jest-dom';
-// import { fireEvent, render, screen } from "@testing-library/react";
-// import HumidityDisplay from '../components/HumidityDisplay';
+import React from "react";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
 
-// // Mock fetch
+import { HumidityDisplay } from "../components/HumidityDisplay";
+import { useBins } from "../contexts/BinContext";
+
+// Mock fetch
 // global.fetch = jest.fn(() =>
-//     Promise.resolve({
-//         json: () => Promise.resolve({ value: "50%" }),
-//     })
+//   Promise.resolve({
+//     json: () => Promise.resolve({ value: "50%" }),
+//   })
 // );
 
-// describe("HumidityDisplay Component", () => {
-//     beforeEach(() => {
-//         fetch.mockClear();
-//     });
+jest.mock("../contexts/BinContext", () => ({
+  useBins: jest.fn(),
+}));
 
-//     test("renders input, button, and initial texts", () => {
-//         render(<HumidityDisplay />);
-//         expect(screen.getByPlaceholderText(/Enter Bin Number/i)).toBeInTheDocument();
-//         expect(screen.getByRole("button", { name: /Get Humidity/i })).toBeInTheDocument();
-//     });
+describe("HumidityDisplay Component", () => {
+  beforeEach(() => {
+    useBins.mockClear();
 
-//     test("changes in input field", () => {
-//         render(<HumidityDisplay />);
-//         const inputEl = screen.getByPlaceholderText(/Enter Bin Number/i);
-//         fireEvent.change(inputEl, { target: { value: '123' } });
-//         expect(inputEl.value).toBe('123');
-//     });
+    useBins.mockReturnValue({
+      getBinHumidity: jest.fn(),
+      currentBinHumidity: 50,
+      isLoading: false,
+      error: null,
+    });
+  });
 
-//     test("displays loading text on fetch call", () => {
-//         render(<HumidityDisplay />);
-//         fireEvent.click(screen.getByRole("button", { name: /Get Humidity/i }));
-//         expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
-//     });
+  test("renders input, button, and initial texts", () => {
+    render(<HumidityDisplay />);
+    expect(
+      screen.getByPlaceholderText(/Enter Bin Number/i)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /Get Humidity/i })
+    ).toBeInTheDocument();
+  });
 
-//     test("displays humidity data on successful fetch", async () => {
-//         render(<HumidityDisplay />);
-//         const button = screen.getByRole("button", { name: /Get Humidity/i });
-//         const inputEl = screen.getByPlaceholderText(/Enter Bin Number/i);
+  test("changes in input field", () => {
+    render(<HumidityDisplay />);
+    const inputEl = screen.getByPlaceholderText(/Enter Bin Number/i);
+    fireEvent.change(inputEl, { target: { value: "123" } });
+    expect(inputEl.value).toBe("123");
+  });
 
-//         fireEvent.change(inputEl, { target: { value: '123' } });
+  /*   test("displays loading text on fetch call", () => {
+    render(<HumidityDisplay />);
+    fireEvent.click(screen.getByRole("button", { name: /Get Humidity/i }));
+    expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
+  }); */
 
-//         fetch.mockResolvedValueOnce({
-//             ok: true,
-//             json: async () => ({ value: "50%" }),
-//         });
+  /*   test("displays humidity data on successful fetch", async () => {
+    render(<HumidityDisplay />);
+    const button = screen.getByRole("button", { name: /Get Humidity/i });
+    const inputEl = screen.getByPlaceholderText(/Enter Bin Number/i);
 
-//         fireEvent.click(button);
+    fireEvent.change(inputEl, { target: { value: "123" } });
 
-//         const humidityText = await screen.findByText(/Humidity Level: 50%/i);
-//         expect(humidityText).toBeInTheDocument();
-//     });
+    fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({ value: "50%" }),
+    });
 
-//     test("displays error message on fetch failure", async () => {
-//         fetch.mockImplementationOnce(() => Promise.reject(new Error("Error fetching data")));
-//         render(<HumidityDisplay />);
-//         const button = screen.getByRole("button", { name: /Get Humidity/i });
+    fireEvent.click(button);
 
-//         fireEvent.click(button);
+    const humidityText = await screen.findByText(/Humidity Level: 50%/i);
+    expect(humidityText).toBeInTheDocument();
+  }); */
 
-//         const errorMessage = await screen.findByText(/Error fetching data/i);
-//         expect(errorMessage).toBeInTheDocument();
-//     });
-// });
+  /*   test("displays error message on fetch failure", async () => {
+    fetch.mockImplementationOnce(() =>
+      Promise.reject(new Error("Error fetching data"))
+    );
+    render(<HumidityDisplay />);
+    const button = screen.getByRole("button", { name: /Get Humidity/i });
+
+    fireEvent.click(button);
+
+    const errorMessage = await screen.findByText(/Error fetching data/i);
+    expect(errorMessage).toBeInTheDocument();
+  }); */
+});
