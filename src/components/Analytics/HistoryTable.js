fde07871a6d14623724587272f2ef6a20347d646
rajipkanxo01 <pdlrajip12@gmail.com>
2023-12-05 21:30:26 +0100

    feat(pagination now works) - added pagination options - can select between 5,10,25 rows of data per page
    References Analytics of Data #49

--- src/components/Analytics/HistoryTable.js
+++ src/components/Analytics/HistoryTable.js
@@ -8,16 +8,40 @@ import {
   TableCell,
   IconButton,
   TableBody,
+  Box,
+  TableFooter,
+  TablePagination,
 } from "@mui/material";
 
 import "../../styles/Analytics_css/HistoryTable.css";
 import { useBins } from "../../contexts/BinContext";
-import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
+import {
+  FirstPage,
+  KeyboardArrowDown,
+  KeyboardArrowLeft,
+  KeyboardArrowRight,
+  KeyboardArrowUp,
+  LastPage,
+} from "@mui/icons-material";
 
 import { Spinner } from "../../components/Spinner";
+import { TablePaginationActions } from "./TablePaginationActions";
 
 function HistoryTable() {
   const { bins, isLoading } = useBins();
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
     <div className="table-container">
       {isLoading ? (
@@ -39,10 +63,28 @@ function HistoryTable() {
 
             {/* table body */}
             <TableBody>
-              {bins.map((bin) => (
+              {(rowsPerPage > 0
+                ? bins.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : bins
+              ).map((bin) => (
                 <Row key={bin.id} value={bin} />
               ))}
             </TableBody>
+
+            <TableFooter>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                count={bins.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableFooter>
           </Table>
         </TableContainer>
       )}
