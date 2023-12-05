f3ef2abf2207fda98f7966841eeaaaf108a621dc
rajipkanxo01 <pdlrajip12@gmail.com>
2023-12-05 17:11:08 +0100

    fixed cloud api for get list of users

--- src/contexts/UserListContext.js
+++ src/contexts/UserListContext.js
@@ -1,24 +1,51 @@
-import { React, createContext, useContext } from "react";
-import { useQuery } from "react-query";
-import { fetchUserInfo } from "../api/FetchFromAPI";
+import { React, createContext, useContext, useEffect, useState } from "react";
+// import { useQuery } from "react-query";
+// import { BASE_URL } from "../contexts/BinContext";
 
 export const UserContext = createContext();
+const BASE_URL = "https://garbage-backend-service-kq2hras2oq-ey.a.run.app";
 
 function UserListProvider({ children }) {
-  const queryKey = "collectorsData";
-  const queryFn = () => fetchUserInfo();
+  const [isLoading, setIsLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const isAuthenticated = Boolean(localStorage.getItem("authenticate"));
+  const token = localStorage.getItem("token");
 
-  const { isLoading, isError, data, refetch } = useQuery(queryKey, queryFn, {
-    staleTime: Infinity,
-  });
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/users`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await res.json();
 
-  const refreshData = async () => {
+        // console.log(isAuthenticated);
+
+        console.log(users.fullname);
+
+        setUsers(data);
+      } catch (e) {
+        alert(e.Message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (isAuthenticated) {
+      fetchUsers();
+    }
+  }, [isAuthenticated, token]);
+
+  /* const refreshData = async () => {
     alert("Refresh");
     await refetch();
-  };
+  }; */
 
   return (
-    <UserContext.Provider value={{ isLoading, isError, data, refreshData }}>
+    <UserContext.Provider value={{ isLoading, users }}>
       {children}
     </UserContext.Provider>
   );
@@ -29,7 +56,6 @@ function useUserListContext() {
   if (context === undefined) {
     throw new Error("user context was outside userlistprovider");
   }
-  // console.log(context);
   return context;
 }
 
