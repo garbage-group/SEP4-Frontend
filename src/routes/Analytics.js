1b013c8ac185a23947a4b6ba1f2038ae84708b25
Pramesh Shrestha <prmsstha22@gmail.com>
2023-12-07 23:35:02 +0100

    feat(bar graph) - created bar graph for temperature
    Reference Analytics of Data #49

--- src/routes/Analytics.js
+++ src/routes/Analytics.js
@@ -6,7 +6,10 @@ import { HistoryTable } from "../components/Analytics/HistoryTable";
 import "../styles/Analytics_css/Analytics.css"
 import GroupIcon from '@mui/icons-material/Group';
 import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
-import { PieChartHumidity } from "../components/utils/PieChartHumidity";
+import BarChartHumidity from "../components/utils/BarChartHumidity";
+import BarChartTemp from "../components/utils/BarChartTemp";
+
+
 
 
 
@@ -19,30 +22,14 @@ export function Analytics() {
 
   return (
     <div>
-      {/* <LineChart
-        xAxis={[
-          {
-            data: [1, 2, 3, 5, 8, 10],
-            label: "X Axis Label",
-          },
-        ]}
-        series={[
-          {
-            data: [2, 5.5, 2, 8.5, 1.5, 5],
-            label: "data",
-          },
-        ]}
-        width={900}
-        height={300}
-      /> */}
       <div className="cards">
-        
         <DataCard title={"Total Municipality Workers"} icon={<GroupIcon className="groupIcon"/>} data={totalAdmin} className={"totalUsers"} /> 
         <DataCard title={"Total Garbage Collectors"} icon={<GroupIcon className="groupIcon"/>} data={totalGarbCollector} className={"totalUsers"} /> 
         <DataCard title={"Total Garbage Bins"} icon={<DeleteSweepIcon className="binIcon"/>} data={totalBins} className={"totalBins"} />
       </div>
-      <PieChartHumidity />
-      <HistoryTable />
+    <BarChartHumidity />
+    <BarChartTemp />
+    <HistoryTable />
 
     </div>
   );
