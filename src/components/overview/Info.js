3eda2de52b33c44b40c4494430324cd2868f0caf
Andreea101018 <andreea.asimine@yahoo.com>
2023-12-08 21:07:29 +0100

    MusicBox Implemented

--- src/components/overview/Info.js
+++ src/components/overview/Info.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 import "../../styles/overview_css/Info.css";
+import "../../styles/overview_css/News.css"
+import { LatestNews } from "../overview/LatestNews"
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
 
 export function Info() {
   return (
     <div className="info-container">
-      <div className="info-box"></div>
-      <div className="info-box"></div>
+      <div id="first-info-box" className="info-box">
+        <MusicNoteIcon className="music-note-icon" />
+        <div className="music-title">Music for You</div>
+        <MusicNoteIcon className="music-note-icon" />
+        <div id="music-container" className="music-container">
+          <LatestNews />
+        </div>
+      </div>
       <div className="info-box"></div>
       <div className="info-box"></div>
     </div>
