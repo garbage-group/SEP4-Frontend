728b051a143e964e84289160c96f33d87f522ccc
rajipkanxo01 <pdlrajip12@gmail.com>
2023-12-10 16:18:10 +0100

    - fixed error in user list context.js

--- src/contexts/UserListContext.js
+++ src/contexts/UserListContext.js
@@ -1,44 +1,39 @@
-import { React, createContext, useContext } from "react";
+import { React, createContext, useContext, useEffect, useState } from "react";
 import { BASE_URL } from "../contexts/BinContext";
-import { useQuery } from "@tanstack/react-query";
 
 export const UserContext = createContext();
 // const BASE_URL = "https://garbage-backend-service-kq2hras2oq-ey.a.run.app";
 
 function UserListProvider({ children }) {
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [users, setUsers] = useState([]);
   const isAuthenticated = Boolean(localStorage.getItem("authenticate"));
   const token = localStorage.getItem("token");
 
-  const { isLoading, data: users } = useQuery({
-    queryKey: ["fetchUsers"],
-    queryFn: fetchUsers,
-    enabled: isAuthenticated && token,
-  });
-
   async function fetchUsers() {
-    const response = await fetch(`${BASE_URL}/users`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error("Error fetching users");
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await res.json();
+      setUsers(data);
+    } catch (e) {
+      alert(e.Message);
+    } finally {
+      setIsLoading(false);
     }
-
-    return response.json();
   }
 
-  /*   useEffect(() => {
+  useEffect(() => {
     if (isAuthenticated) {
       fetchUsers();
     }
     // fetchUsers is defined inside UserListProvider and is not changing,
     // so it's safe to exclude it from the dependency array
-  }, [isAuthenticated, token]); */
+  }, [isAuthenticated, token]);
 
   return (
     <UserContext.Provider value={{ isLoading, users, fetchUsers }}>
